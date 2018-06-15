11664a82359ace1c9d6d8b2e38f0e10ecbbc5ad2
reggiepangilinan <reggie.pangilinan@stationfive.com>
2018-06-15 15:32:01 +1000

    Correct linting issues

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const mapReduceSqlRows = require('.src/mapReduceSqlRows');
-const addSqlParamsToRequest = require('.src/addSqlParamsToRequest');
-const promisifyTediousExecSql = require('.src/promisifyTediousExecSql');
-const addParam = require('.src/addParams');
+const mapReduceSqlRows = require('./src/mapReduceSqlRows');
+const addSqlParamsToRequest = require('./src/addSqlParamsToRequest');
+const promisifyTediousExecSql = require('./src/promisifyTediousExecSql');
+const addParam = require('./src/addParams');
 
 module.exports = {
   mapReduceSqlRows,
