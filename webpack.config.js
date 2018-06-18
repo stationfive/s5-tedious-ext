b304810bfffb18b384cb0c6f3f8d3a57cb749d19
reggiepangilinan <reggie.pangilinan@stationfive.com>
2018-06-18 10:51:44 +1000

    Set mode to prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: 'index.js',
   target: 'node',
   externals: [nodeExternals()],
