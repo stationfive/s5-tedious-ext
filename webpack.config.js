19725157ae77819e4e4a823736975c90d75cfd61
reggiepangilinan <reggie.pangilinan@stationfive.com>
2018-06-18 10:57:18 +1000

    Set mode back to dev and remove target node

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,8 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: 'index.js',
-  target: 'node',
   externals: [nodeExternals()],
   output: {
     path: path.resolve(__dirname, 'dist'),
