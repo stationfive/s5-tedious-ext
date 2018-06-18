29a723e23bddb161d5737bd2677ab1dedfe5870d
reggiepangilinan <reggie.pangilinan@stationfive.com>
2018-06-18 11:10:43 +1000

    Remove nodeexternals

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: 'index.js',
-  externals: [nodeExternals()],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
